640c5c3b9919a4e1cbb0b9710bbfb3499e483756
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-05-31 17:54:07 +0200

    :construction_worker:

--- terraform/database_user/main.tf
+++ terraform/database_user/main.tf
@@ -3,16 +3,13 @@ resource "random_id" "user-password" {
 }
 
 resource "google_sql_user" "default" {
-  # count    = var.master_instance_name == "" ? 1 : 0
   name     = var.database_user["user_name"]
   project  = var.database_user["project"]
   instance = var.database_user["db_instance_name"]
-  # host     = lookup(var.database_user, "user_host", "%")
   password = var.database_user_password == "" ? random_id.user-password.hex : var.database_user_password
 }
 
 resource "google_sql_database" "default" {
-  # count     = var.master_instance_name == "" ? 1 : 0
   name      = var.database_user["db_name"]
   project   = var.database_user["project"]
   instance  = var.database_user["db_instance_name"]
